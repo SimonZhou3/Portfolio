1ffb52899dd9805256e728ce55ef47c4e8b6e8a6
Simon <simonzhou98@gmail.com>
2024-11-26 10:32:45 -0800

    add new experience

--- src/gatsby-theme-portfolio-minimal/sections/Experience/index.tsx
+++ src/gatsby-theme-portfolio-minimal/sections/Experience/index.tsx
@@ -5,7 +5,6 @@ import 'react-vertical-timeline-component/style.min.css';
 import {Animation} from '../../components/Animation';
 import {Section} from '../../components/Section';
 import {PageSection} from '../../types';
-import StGeorgesImage from '../../../../content/images/st-georges.jpg';
 
 import * as classes from './style.module.css';
 
@@ -25,6 +24,33 @@ export default function ExperienceSection(props: PageSection): React.ReactElemen
                         iconStyle={{background: 'rgb(204,9,47)', color: '#fff'}}
                     >
                         <h3 className={`vertical-timeline-element-title ${classes.Title}`}>Software Developer Co-op </h3>
+                        <h4 className={`vertical-timeline-element-subtitle ${classes.Title}`}>Global Relay</h4>
+                        <ul className={classes.Responsibilities}>
+                            <li>Contributed towards the development of a distributed message compliance software using Java, Kafka, Cassandra, and Docker,
+                                used by over 20,000 customers daily, to archive 1 TB messages/day
+                            </li>
+                            <li>Improved code infrastructure and scalability by developing a on-disk cache to reduce the load of RPC calls between services
+                                storing intangible data as compressed bytes and securing data with AES-256-GCM encryption
+                            </li>
+                            <li> Developed a Fabric deployment script for a microservice, enabling developers to deploy builds to test and production environments
+                            </li>
+                            <li> Improved the build pipeline on Jenkins by integrating automated formatting checks and implementing logging for flaky tests,
+                                enhancing build reliability and debugging efficiency
+                            </li>
+                        </ul>
+                        <p>
+                            Java, Typescript, Jenkins, Kafka
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{background: 'rgb(204,9,47)', color: '#fff'}}
+                        contentArrowStyle={{borderRight: '7px solid  rgb(204,9,47)'}}
+                        date="January 2022 - June 2022"
+                        dateClassName={classes.Date}
+                        iconStyle={{background: 'rgb(204,9,47)', color: '#fff'}}
+                    >
+                        <h3 className={`vertical-timeline-element-title ${classes.Title}`}>Junior Software Developer Co-op </h3>
                         <h4 className={`vertical-timeline-element-subtitle ${classes.Title}`}>St. George's School</h4>
                             <ul className={classes.Responsibilities}>
                                 <li> Participated in the development of a comprehensive full-stack web application,
